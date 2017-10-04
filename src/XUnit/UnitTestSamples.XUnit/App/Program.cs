faa13b2f39f7ea84211a5f5568c255c6c1ab54dd
Per Samuelsson <per@starcounter.com>
2017-10-04 14:42:50 +0200

    Initial setup of target test projects (for xUnit).

---
+++ src/XUnit/UnitTestSamples.XUnit/App/Program.cs
@@ -0,0 +1,17 @@
+﻿using Starcounter;
+using System;
+
+namespace App {
+
+    [Database] public class Started {
+        public DateTime Time { get; set; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Db.Transact(() => {
+                new Started() { Time = DateTime.UtcNow };
+            });
+        }
+    }
+}
---
+++ src/XUnit/UnitTestSamples.XUnit/AppLibrary/Person.cs
@@ -0,0 +1,12 @@
+﻿using Starcounter;
+
+namespace AppLibrary {
+
+    [Database]
+    public class Person {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+    }
+}
