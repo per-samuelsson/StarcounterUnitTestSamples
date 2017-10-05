cc08441096697f41ed16cc3a678c659440a81575
Per Samuelsson <per@starcounter.com>
2017-10-05 13:58:41 +0200

    Introduced simple framework for tests to run in a context (supporting hosting).

---
+++ src/XUnit/UnitTestSamples.XUnit/App.Tests/ContextualTest.cs
@@ -0,0 +1,12 @@
+﻿
+using Xunit;
+
+namespace App.Tests {
+
+    [CollectionDefinition(nameof(TestContext))]
+    public class TestContextCollection : ICollectionFixture<TestContext> { }
+
+    [Collection(nameof(TestContext))]
+    public abstract class ContextualTest {
+    }
+}
---
+++ src/XUnit/UnitTestSamples.XUnit/App.Tests/TestContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Starcounter;
+using Starcounter.Hosting;
+using System;
+using System.Linq;
+
+namespace App.Tests {
+
+    public class TestContext : IDisposable {
+        readonly ICodeHost host;
+
+        public string DatabaseName { get; } = typeof(TestContext).Assembly.GetName().Name.Replace(".", "");
+
+        public TestContext() {
+            new TestPrerequisites(DatabaseName)
+                .StartServer()
+                .StopHost()
+                .RecreateDatabase();
+
+            var h = new AppHostBuilder()
+                .UseDatabase(DatabaseName)
+                .UseApplication(typeof(App.Program).Assembly)
+                .Build();
+            h.Start();
+            host = h;
+        }
+
+        void IDisposable.Dispose() {
+            host.Dispose();
+        }
+    }
+}
